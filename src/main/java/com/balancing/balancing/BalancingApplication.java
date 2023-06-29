dc7630681719b9f26da9ebdf73daa3e080e5b07f
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-29 11:58:04 -0600

    Actualizando para usar spring cloud gateway

--- src/main/java/com/balancing/balancing/BalancingApplication.java
+++ src/main/java/com/balancing/balancing/BalancingApplication.java
@@ -1,48 +1,12 @@
 package com.balancing.balancing;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-@RestController
 public class BalancingApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(BalancingApplication.class);
-
-	private final WebClient.Builder loadBalancedWebClientBuilder;
-	private final ReactorLoadBalancerExchangeFilterFunction lbFunction;
-
-	public BalancingApplication(WebClient.Builder webClientBuilder,
-												 ReactorLoadBalancerExchangeFilterFunction lbFunction) {
-		this.loadBalancedWebClientBuilder = webClientBuilder;
-		this.lbFunction = lbFunction;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BalancingApplication.class, args);
 	}
 
-	@RequestMapping("/hi")
-	public Mono<String> hi(@RequestParam(value = "name", defaultValue = "Mary") String name) {
-		return loadBalancedWebClientBuilder.build().get().uri("http://say-hello/greeting")
-			.retrieve().bodyToMono(String.class)
-			.map(greeting -> String.format("%s, %s!", greeting, name));
-	}
-
-	@RequestMapping("/hello")
-	public Mono<String> hello(@RequestParam(value = "name", defaultValue = "John") String name) {
-		return WebClient.builder()
-			.filter(lbFunction)
-			.build().get().uri("http://say-hello/greeting")
-			.retrieve().bodyToMono(String.class)
-			.map(greeting -> String.format("%s, %s!", greeting, name));
-	}
 }
