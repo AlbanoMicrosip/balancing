f275ac028b0ec5122eceabf2cac5eda5c439e424
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-28 21:21:48 -0600

    Retornando a punto estable

--- src/main/java/com/balancing/balancing/SayHelloConfigurationTwo.java
+++ src/main/java/com/balancing/balancing/SayHelloConfigurationTwo.java
@@ -3,14 +3,12 @@ package com.balancing.balancing;
 
 import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.Request;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
 import com.balancing.balancing.util.DNSUtil;
 
 import java.net.InetAddress;
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,20 +36,24 @@ class DnsServiceInstanceListSupplier implements ServiceInstanceListSupplier {
 
   @Override
   public Flux<List<ServiceInstance>> get() {
-    System.out.println("Entrando a Get");
-    return Flux.interval(Duration.ofSeconds(5))  // emits every 5 seconds
-      .onBackpressureDrop()  // drops emissions if the downstream can't keep up
-      .flatMap(tick -> {
-        try {
-          System.out.println("Dentro de Try");
-          List<InetAddress> hosts = DNSUtil.resolve(serviceId);
-          List<ServiceInstance> servers = hosts.stream().map(
-            host -> new DefaultServiceInstance(serviceId + Math.random() * 100, serviceId, host.getHostAddress(), port, false)
-          ).collect(Collectors.toList());
-          return Flux.just(servers);
-        } catch (Exception e) {
-          return Flux.error(e);
+    try {
+
+      List<InetAddress> hosts = DNSUtil.resolve(serviceId);
+      System.out.println("Despues de resolver");
+      for (InetAddress inet:hosts) {
+        System.out.println(inet.toString());
+      }
+
+      List<ServiceInstance> servers = hosts.stream().map(
+        host -> {
+          return new DefaultServiceInstance(serviceId+host.hashCode(), serviceId, host.getHostAddress(), port, false);
         }
-      });
+      ).collect(Collectors.toList());
+//      servers.add(new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 8090, false));
+      return Flux.just(servers);
+    } catch (Exception e) {
+      return Flux.error(e);
+    }
   }
+
 }
