a5da95822fe90958d15de38c4ad4b1058b6bc5fb
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-29 10:11:12 -0600

    Probando load balancing a como estaba antes

--- src/main/java/com/balancing/balancing/util/DNSUtil.java
+++ src/main/java/com/balancing/balancing/util/DNSUtil.java
@@ -23,8 +23,8 @@ public class DNSUtil {
 
   public static List<InetAddress> resolve(String host) {
     try {
-      Lookup.getDefaultCache(DClass.IN).setMaxCache(0);
-      Lookup.getDefaultCache(DClass.IN).setMaxNCache(0);
+//      Lookup.getDefaultCache(DClass.IN).setMaxCache(30);
+//      Lookup.getDefaultCache(DClass.IN).setMaxNCache(30);
       Lookup lookup = new Lookup(host);
       Record[] result = lookup.run();
 
