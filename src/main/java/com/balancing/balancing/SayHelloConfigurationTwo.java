b830957575935eb1a34baed4c187d7f22d6934ed
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-28 20:46:31 -0600

    Agregando codigo

---
+++ src/main/java/com/balancing/balancing/SayHelloConfigurationTwo.java
@@ -0,0 +1,60 @@
+package com.balancing.balancing;
+
+
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+import com.balancing.balancing.util.DNSUtil;
+
+import java.net.InetAddress;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SayHelloConfigurationTwo {
+  @Bean
+  ServiceInstanceListSupplier serviceInstanceListSupplier() {
+    return new DnsServiceInstanceListSupplier("say-hello", 8090);
+  }
+}
+
+class DnsServiceInstanceListSupplier implements ServiceInstanceListSupplier {
+
+  private final String serviceId;
+  private final int port;
+
+  public DnsServiceInstanceListSupplier(String serviceId, int port) {
+    this.serviceId = serviceId;
+    this.port = port;
+  }
+
+  @Override
+  public String getServiceId() {
+    return serviceId;
+  }
+
+  @Override
+  public Flux<List<ServiceInstance>> get() {
+    try {
+
+      List<InetAddress> hosts = DNSUtil.resolve(serviceId);
+      System.out.println("Despues de resolver");
+      for (InetAddress inet:hosts
+           ) {
+        System.out.println(inet.toString());
+      }
+
+      List<ServiceInstance> servers = hosts.stream().map(
+        host -> {
+          return new DefaultServiceInstance(serviceId+host.hashCode(), serviceId, host.getHostAddress(), port, false);
+        }
+      ).collect(Collectors.toList());
+//      servers.add(new DefaultServiceInstance(serviceId + "1", serviceId, "localhost", 8090, false));
+      return Flux.just(servers);
+    } catch (Exception e) {
+      return Flux.error(e);
+    }
+  }
+
+}
---
+++ src/main/java/com/balancing/balancing/util/DNSUtil.java
@@ -0,0 +1,50 @@
+package com.balancing.balancing.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xbill.DNS.*;
+
+import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Microsip Platform as a Service
+ * Copyright Aplicaciones y Proyectos Computacionales, S. A. de C. V. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Created by paco <paco@technogi.com.mx> on 15/06/18.
+ */
+public class DNSUtil {
+
+  private static final Logger log = LoggerFactory.getLogger(DNSUtil.class);
+
+  public static List<InetAddress> resolve(String host) {
+    try {
+      Lookup lookup = new Lookup(host);
+      Record[] result = lookup.run();
+
+      if (result == null || lookup.getResult() != Lookup.SUCCESSFUL) {
+        log.debug("result is " + result);
+        log.debug("lookup.getResult() is " + lookup.getResult());
+
+        // Reseteamos el caché para evitar error cuando se reinicia el servicio al que se está intentando resolver
+        Cache  defaultCache = Lookup.getDefaultCache(DClass.IN);
+        defaultCache.clearCache();
+
+        return Collections.emptyList();
+      }
+
+      List<InetAddress> hosts = Arrays.asList(result).stream()
+        .map(record -> {
+          InetAddress hostAdress = ((ARecord) record).getAddress();
+          return hostAdress;
+        }).collect(Collectors.toList());
+      return hosts;
+    } catch (Exception ex) {
+      return null;
+    }
+  }
+}
