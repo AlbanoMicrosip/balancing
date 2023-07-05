dd4e77c5438d4cfac73353749f67b3bf7f3c9090
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-05 15:01:46 -0600

    Obtener dinamicamente uri

--- src/main/java/com/balancing/balancing/BalancingController.java
+++ src/main/java/com/balancing/balancing/BalancingController.java
@@ -1,22 +1,36 @@
 package com.balancing.balancing;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 public class BalancingController {
 
   private final RestTemplate restTemplate;
+  private final DiscoveryClient discoveryClient;
 
-  @Autowired
-  public BalancingController(RestTemplate restTemplate) {
+  public BalancingController(RestTemplate restTemplate, DiscoveryClient discoveryClient) {
     this.restTemplate = restTemplate;
+    this.discoveryClient = discoveryClient;
   }
 
   @GetMapping("/call-say-instance")
   public String callSayInstance() {
-    return restTemplate.getForObject("http://localhost:8080/redireccion/say", String.class);
+    List<InstanceInfo> instances = discoveryClient.getInstancesById("SAY-INSTANCE");
+    if (instances != null && !instances.isEmpty()) {
+      String uri = instances.get(0).getHomePageUrl();
+      System.out.println("URL:::::::");
+      System.out.println(uri);
+      return restTemplate.getForObject(uri + "/", String.class);
+    } else {
+      return "No service instance available.";
+    }
   }
 }
