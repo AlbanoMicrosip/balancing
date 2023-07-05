647c488fdcd8e096ca511bc4f73d9ca3d59f6319
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-05 13:56:49 -0600

    Ver si es detectado por api-gateway

--- src/main/java/com/balancing/balancing/BalancingApplication.java
+++ src/main/java/com/balancing/balancing/BalancingApplication.java
@@ -2,8 +2,12 @@ package com.balancing.balancing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class BalancingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BalancingApplication.class, args);
