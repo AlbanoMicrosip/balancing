b19b093f588d1672a33cee885dff6e2067203149
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-06 17:30:40 -0600

    Viendo si funciona

--- src/main/java/com/balancing/balancing/BalancingApplication.java
+++ src/main/java/com/balancing/balancing/BalancingApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class BalancingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BalancingApplication.class, args);
