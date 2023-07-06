a9eaf8e262b6beaf8a2373dee7fb0e9559c34874
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-06 16:26:24 -0600

    Obtener dinamicamente uri

--- src/main/java/com/balancing/balancing/BalancingApplication.java
+++ src/main/java/com/balancing/balancing/BalancingApplication.java
@@ -3,13 +3,11 @@ package com.balancing.balancing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class BalancingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BalancingApplication.class, args);
--- src/main/java/com/balancing/balancing/BalancingController.java
+++ src/main/java/com/balancing/balancing/BalancingController.java
@@ -1,39 +1,26 @@
 package com.balancing.balancing;
 
-import com.netflix.appinfo.InstanceInfo;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.util.List;
 
 @RestController
 public class BalancingController {
 
+  @LoadBalanced
   private final RestTemplate restTemplate;
-  private final DiscoveryClient discoveryClient;
 
   @Autowired
-  public BalancingController(RestTemplate restTemplate, DiscoveryClient discoveryClient) {
+  public BalancingController(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
-    this.discoveryClient = discoveryClient;
   }
 
   @GetMapping("/call-say-instance")
   public String callSayInstance() {
-    List<ServiceInstance> instances = discoveryClient.getInstances("SAY-INSTANCE");
-    if (instances != null && !instances.isEmpty()) {
-      URI uri = instances.get(0).getUri();
-      System.out.println("URL:::::::");
-      System.out.println(uri.toString());
-      return restTemplate.getForObject(uri + "/", String.class);
-    } else {
-      return "No service instance available.";
-    }
+      return restTemplate.getForObject("http://SAY-INSTANCE" + "/", String.class);
   }
 
   @GetMapping("/call-say-instance-fix")
