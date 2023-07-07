632ccb83b3daa2e2dd015d250d31cd87227b7727
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-07-07 15:54:08 -0600

    Intentando incrustar controlador

--- src/main/java/com/balancing/balancing/BalancingApplication.java
+++ src/main/java/com/balancing/balancing/BalancingApplication.java
@@ -3,7 +3,10 @@ package com.balancing.balancing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +16,10 @@ public class BalancingApplication {
 		SpringApplication.run(BalancingApplication.class, args);
 	}
 
+//	@LoadBalanced
+//	@Bean
+//	WebClient.Builder webClientBuilder() {
+//		return WebClient.builder();
+//	}
 
 }
---
+++ src/main/java/com/balancing/balancing/BalancingController.java
@@ -0,0 +1,25 @@
+package com.balancing.balancing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class BalancingController {
+  private final WebClient.Builder webClientBuilder;
+
+  @Autowired
+  public BalancingController(WebClient.Builder webClientBuilder) {
+    this.webClientBuilder = webClientBuilder;
+  }
+
+  @GetMapping("/loadbalanced")
+  public Mono<String> getLoadBalanced() {
+    return webClientBuilder.build().get()
+      .uri("http://SAY-INSTANCE/")
+      .retrieve()
+      .bodyToMono(String.class);
+  }
+}
